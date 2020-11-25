37eaefbe5ed446a90385e1b207c48a3935cdbcbf
Aaron Bill B. Domingo <domingoaaronbill@gmail.com>
2020-11-25 20:02:03 +0800

    try curl get id, username

--- .github/scripts/instagram.sh
+++ .github/scripts/instagram.sh
@@ -1,7 +1,2 @@
-curl -X POST \
-  https://api.instagram.com/oauth/access_token \
-  -F client_id=2805644083092506 \
-  -F client_secret=1ce1bfc5e8ac241c8a39b0f93b6b4adf \
-  -F grant_type=authorization_code \
-  -F redirect_uri=https://aabill.github.io/Aabill/ \
-  -F code=AQCjdF2qtzeYuJ7tNfv_iWTKs-zFT_NnsS_NJvesL__Yhga7DF3521KAzOzfFzTM69NDLRCnia5p-3YFFZU9kJxhvOnWZq-KP-bR6t2Itk4ll6RVGRjxTIRaEtAZXHfv62iLG4UsHANnA2yJb815TW0BRZLTG1uMBQ1ACgp0ts_xO5eC_pJOIcxwdcYX9QBd0TRAp8gh9krpdMLm3rWa1x27ovI4UPoDtjeoei1IUNzzQA
+curl -X GET \
+  'https://graph.instagram.com/me?fields=id,username&access_token=IGQVJXaUc3WXBGX3JneWgwWmw5S3lSeklKY2EwRUk4NFJmS2lHTmFkQ2lINGJRa3lPZAzBlZAWE2b2hsWno3THl4aHVEal9tYTlvazI3cHB6eGpac2h0X0dYcmd6czZAkRTBoQlY2UENCbXlUbG1peFhRdQZDZD'
