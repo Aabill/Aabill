1f1cb11dc8f98a9c37919e60b586cbc0bb124a34
Aaron Bill B. Domingo <domingoaaronbill@gmail.com>
2020-11-27 15:35:40 +0800

    Try this

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -22,4 +22,29 @@ axios.get('https://graph.instagram.com/me/media', {
     console.log("Done Axios Get Request.");
   });  
 
-  write.sync('README.md', 'testing write some data...', { newline: true }); 
+let README = `
+### Hi there 👋
+
+ ---
+ 
+- 🔭 I’m currently working on my Github Readme Profile and getting my hands dirty with Github Workflows.
+- 🌱 I’m currently learning [Google Cloud Storage](http://cloud.google.com/storage/) and [Laravel Jetstream Inertia](http://jetstream.laravel.com/1.x/stacks/inertia.html).
+- 👯 I’m looking to collaborate on something I haven't thought of yet.
+- 🤔 I’m looking for help with my lovelife.
+- 💬 Ask me about anything but lovelife.
+- 😄 Pronouns: he / him / his
+- ⚡ Fun fact: I can make bubbles using my saliva.
+
+
+#### Languages and tools
+<img align="left" alt="Visual Studio Code" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" /><img align="left" alt="html5" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" /><img align="left" alt="css3" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" /><img align="left" alt="sass" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sass/sass.png" /><img align="left" alt="javascript" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" /><img align="left" alt="npm" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/npm/npm.png" /><img align="left" alt="webpack" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/webpack/webpack.png" /><img align="left" alt="vue" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vue/vue.png" /><img align="left" alt="php" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/php/php.png" /><img align="left" alt="laravel" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/laravel/laravel.png" /><img align="left" style="display: inline-block" alt="git" width="20px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png" />
+
+<br/>
+
+##  
+
+### 📫 <domingoaaronbill@gmail.com>
+[![Aabill's github stats](https://github-readme-stats.vercel.app/api?username=Aabill)](https://github.com/Aabill/github-readme-stats)
+`
+
+  write.sync('README.md', `${README} <br/> testing write some data...`, { newline: true }); 
