0b46114096752c1408b100a0002597cfdd90c8c4
Aaron Bill B. Domingo <domingoaaronbill@gmail.com>
2020-11-27 13:44:27 +0800

    Test Fetch Request

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -5,4 +5,17 @@ dotenv.config();
 
 console.log(process.env.PASSWORD);
 
-console.log(process.env.IG_TOKEN);
+let promise = fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=${process.env.IG_TOKEN}`);
+
+promise.then(response => {
+    if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+    }
+    response.json().then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.log(error.message);
+    })
+})
