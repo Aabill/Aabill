fd5d0cc2e765152263bc915f2df52bfa2e476c09
Aaron Bill B. Domingo <domingoaaronbill@gmail.com>
2020-11-27 15:01:30 +0800

    Add gitignore and write something test

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -20,3 +20,19 @@ axios.get('https://graph.instagram.com/me/media', {
     // always executed
     console.log("Done Axios Get Request.");
   });  
+
+function WriteFile()
+{
+
+var fh = fopen("../../test.md", 3); // Open the file for writing
+
+if(fh!=-1) // If the file has been successfully opened
+{
+    var str = "Some text goes here...";
+    fwrite(fh, str); // Write the string to a file
+    fclose(fh); // Close the file
+}
+console.log("File not opened.")
+}
+
+WriteFile();
